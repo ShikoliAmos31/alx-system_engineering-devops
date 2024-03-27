79cb6488c32960a081092127378c06fa83873b75
shikoliamos31 <amosshikoli@gmail.com>
2024-03-27 12:50:21 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Check if the user provided a URL argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request to the URL, and store the HTTP status code in response
-response=$(curl -s -o /dev/null -w "%{http_code}" "${1}")
-
-# Check if the HTTP status code is 200
-if [ "$response" -eq 200 ]; then
-    # If the status code is 200, display the body of the response
-    curl -s -L "${1}"
-else
-    # If the status code is not 200, print an error message
-    echo "Non-200 status code received: $response"
-fi
+# Sends a GET request to the URL, and displays the body of the response
+curl -s -L "${1}"
