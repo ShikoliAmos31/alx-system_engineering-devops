f55d70b1cf89f18da321d044a9a0db3046bc8970
shikoliamos31 <amosshikoli@gmail.com>
2024-05-07 11:59:47 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
