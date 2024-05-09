23749932fe8ea2d91c7a80d25a91ba56042e0ed7
shikoliamos31 <amosshikoli@gmail.com>
2024-05-09 07:15:56 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent":"Amos/5.0"}
+    headers = {"User-Agent":'MyBot/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
